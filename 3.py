e670ad7cf00d008712743027e0e36f3a1700424a
Alyona-76 <alyo-na76@yandex.ru>
2024-07-19 13:39:30 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,7 @@
+name = 'Алена'
+course = 'курс "Python"'
+tasks = 'домашние задания 12'
+watch = 'затрачено часов 1.5'
+print( 1.5 / 12)
+watch1 = 'среднее время на задание 0.125'
+print (name, (';'),course, (';'), tasks, (';'), watch, (';'),  watch1 )
